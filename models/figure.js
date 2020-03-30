9d1ca03efa72b1f1571a03aecf214540c0ea2b47
maxsynnott <maxryansynnott@gmail.com>
2020-03-31 12:22:48 +1300

    switched Figure to object from class

--- models/figure.js
+++ models/figure.js
@@ -1,24 +1,22 @@
 const pool = require('../pool')
 
-const Figure = class {
-	constructor(description, number) {
-		this.description = description;
-		this.number = number;
-	}
 
-	static all() {
-		return pool.query(`
-			SELECT * FROM figures
-			ORDER BY id ASC
-		`)
-	}
+const Figure = {}
 
-	static create(description, number) {
-		return pool.query(`
-			INSERT INTO figures (description, number)
-			VALUES ($1, $2)
-		`, [description, number])
-	}
+
+Figure.all = () => {
+	return pool.query(
+		`SELECT * FROM figures
+		 ORDER BY id ASC`
+	)
+}
+
+Figure.create = (description, number) => {
+	return pool.query(
+		`INSERT INTO figures (description, number)
+		 VALUES ($1, $2)`,
+		[description, number]
+	)
 }
 
 module.exports = Figure
